061b8f7fe0f03cbda08e6d55db262831ecb92d31
R2DMobz <69566022+R2DMobz@users.noreply.github.com>
2024-03-08 18:39:47 -0500

    SHA-3 Python Program, My Final Update For Now
    I made this following documentation that I posted in the resources tab. Please note that it lacks the padding step that needs to be implemented but it should work fine as the core algorithm for the SHA-3 component of our project with a little work. Please test it out and let me know if it is working right, Python is acting a fool on my end once more. If any logic needs to be changed it is likely the axis logic for the arrays but this should work fine for generating hashes.

--- SHA_3.py
+++ SHA_3.py
@@ -3,30 +3,53 @@ rate = 1088
 capacity = 512
 output_size = 256
 
-
-## Generate initial input string of block size 1088 for 256 bit encryption purposes
-plaintext = [1] * rate
-
-## Generate 1600 bit initial state of zeros
-state_array = [0] * (rate + capacity)
-
-## Generate Keccak Function Matrix of size 5 x 5 x 64 (1600 bits initialized at zero)
-state_matrix = [[[0] * 64] * 5] * 5
+## Generate initial input string of block size 1088 for 256 bit encryption purposes (for testing initialized at 1)
+r_block = [1] * rate
 
 ## Rho array for rho step
 rho_array = [[0, 1, 190, 28, 91], [36, 300, 6, 55, 276], [3, 10, 171, 153, 231], [105, 45, 15, 21, 136], [210, 66, 253, 120, 78]]
 
-def sponge(plaintext, state_array, ):
-	
+## Round constant array for iota step
+RC = [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0],[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,1,0,1,0],[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,1,0,1,1],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1],[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,1],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,1,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,1],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,1,0,1,1],[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,1,1],[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,1,0,0,1],[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1],[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0],[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0],[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0],[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1],[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0]]
 
-def keccak():
+def sponge(r_block):
+	
+	## Generate Keccak Function Matrix of size 5 x 5 x 64 (1600 bits initialized at zero)
+	state_matrix = [[[0] * 64] * 5] * 5
+	
+	## Generate empty output block for our results
+	output_hash = [] * (rate + capacity)
+	
+	## Performs the XOR operation between input segment and the initial state block
+	for z in range(64):
+		for x in range(5):
+			for y in range(5):
+				r_position = (64* (x + y) + z)
+				if r_position >= rate:
+					state_matrix[x][y][z] = state_matrix[x][y][z]
+				else:
+					state_matrix[x][y][z] = state_matrix[x][y][z] ^ r_block[r_position]
+	
+	## Perform the Keccak Function operations theta through rho 24 times
 	for i_rate in range(24):
-		A 
-
-def keccak_round():
+		theta_out = theta(state_matrix)
+		rho_out = rho(theta_out, rho_array)
+		pi_out = pi(rho_out)
+		chi_out = chi(pi_out)
+		state_matrix = iota(chi_out, RC, i_rate)
 	
-
-
+	## Convert the matrix into a single line array for use practical use, using a counter to keep track of the new string position
+	output_count = 0
+	for y in range(4):
+		for x in range(4):
+			for z in range(64):
+				output_hash[output_count] = state_matrix[x][y][z]
+				output_count = output_count + 1
+	
+	## Display test output
+	print(output_hash)
+		
+## Theta function as per documentation
 def theta(A):
 	C = [[] * 64] * 5
 	D = [[] * 64] * 5
@@ -43,6 +66,7 @@ def theta(A):
 				A_out[x][y][z] = A[x][y][z] ^ A[x][z]
 	return A_out
 
+## Rho function as per documentation
 def rho(A, rho_array):
 	A_out = [[[] * 64] * 5] * 5
 	for z in range(64):
@@ -52,6 +76,7 @@ def rho(A, rho_array):
 				A_out[x][y][z_out] = A[x][y][z]
 	return A_out
 
+# Pi function as per documentation
 def pi(A):
 	A_out = [[[] * 64] * 5] * 5
 	for z in range(64):
@@ -60,6 +85,7 @@ def pi(A):
 				A_out[x][y][z] = A[(x + (3 * y)) % 5][x][z]
 	return A_out
 
+## Chi function as per documentation
 def chi(A):
 	A_out = [[[] * 64] * 5] * 5
 	for x in range(5):
@@ -67,9 +93,12 @@ def chi(A):
 			A_out[x][y][z] = A[x][y][z] ^ ((not A[(x + 1) % 5][y][z]) and A[(x + 2) % 5][y][z])
 	return A_out
 
-def iota(A, RC):
+## Iota function as per documentation
+def iota(A, RC, i):
 	A_out = [[[] * 64] * 5] * 5
 	for z in range(64):
 		for x in range(5):
 			for y in range(5):
-				A_out[x][y][z] = A[x][y][z] ^ RC
+				## NOTE: The Round Constant array is searched through in reverse direction due to the fact that the binary numbers found would process in the opposite direction of the proper sequence of input values
+				A_out[x][y][z] = A[x][y][z] ^ RC[i][63 - z]
+	return A_out
