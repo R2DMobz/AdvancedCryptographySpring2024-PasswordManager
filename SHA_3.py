1bc55c53f0841bb7bdd1bd0f5573d3f894877455
R2DMobz <69566022+R2DMobz@users.noreply.github.com>
2024-03-08 15:59:57 -0500

    SHA-3 Algorithm
    Now working on Iota function, need to make a round constant table

--- SHA_3.py
+++ SHA_3.py
@@ -13,6 +13,9 @@ state_array = [0] * (rate + capacity)
 ## Generate Keccak Function Matrix of size 5 x 5 x 64 (1600 bits initialized at zero)
 state_matrix = [[[0] * 64] * 5] * 5
 
+## Rho array for rho step
+rho_array = [[0, 1, 190, 28, 91], [36, 300, 6, 55, 276], [3, 10, 171, 153, 231], [105, 45, 15, 21, 136], [210, 66, 253, 120, 78]]
+
 def sponge(plaintext, state_array, ):
 	
 
@@ -27,7 +30,7 @@ def keccak_round():
 def theta(A):
 	C = [[] * 64] * 5
 	D = [[] * 64] * 5
-	A_out = [[[0] * 64] * 5] * 5
+	A_out = [[[] * 64] * 5] * 5
 	for z in range(64):
 		for x in range(5):
 			C[x][z] = A[x][0][z] ^ A[x][1][z] ^ A[x][2][z] ^ A[x][3][z] ^ A[x][4][z]
@@ -40,16 +43,33 @@ def theta(A):
 				A_out[x][y][z] = A[x][y][z] ^ A[x][z]
 	return A_out
 
-def rho(A):
-	
+def rho(A, rho_array):
+	A_out = [[[] * 64] * 5] * 5
+	for z in range(64):
+		for x in range(5):
+			for y in range(5):
+				z_out = rho_array[x][y] % 64
+				A_out[x][y][z_out] = A[x][y][z]
+	return A_out
 
-def pi():
-	
+def pi(A):
+	A_out = [[[] * 64] * 5] * 5
+	for z in range(64):
+		for x in range(5):
+			for y in range(5):
+				A_out[x][y][z] = A[(x + (3 * y)) % 5][x][z]
+	return A_out
 
-def chi():
-	
+def chi(A):
+	A_out = [[[] * 64] * 5] * 5
+	for x in range(5):
+		for y in range(5):
+			A_out[x][y][z] = A[x][y][z] ^ ((not A[(x + 1) % 5][y][z]) and A[(x + 2) % 5][y][z])
+	return A_out
 
-def iota():
-	
-
-
+def iota(A, RC):
+	A_out = [[[] * 64] * 5] * 5
+	for z in range(64):
+		for x in range(5):
+			for y in range(5):
+				A_out[x][y][z] = A[x][y][z] ^ RC
